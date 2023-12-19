c96f315ab7df301edb61d88a623ef62213623967
zeberity123 <hyun.kim.2018@live.rhul.ac.uk>
2023-12-19 10:04:42 +0900

    added day1 material

---
+++ 12180930/devenv.bat
@@ -0,0 +1,5 @@
+@rem
+@rem	set for windows 10
+@rem
+
+	@set PATH=%PATH%;C:\Program Files (x86)\Dev-Cpp\MinGW64\bin;C:\Program Files (x86)\Dev-Cpp\MinGW64\libexec\gcc\x86_64-w64-mingw32\4.9.2;C:\Program Files (x86)\Dev-Cpp\MinGW64\x86_64-w64-mingw32\bin;
